7509bc75a3630392571a7a058cce585706be921e
Ali Mahmoud <ali.mahmoud.work@gmail.com>
2022-05-14 11:24:54 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 var quotes = [
   `“Be yourself; everyone else is already taken.”<br>
-― Oscar Wilde `,
+--Oscar Wilde `,
   `“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”<br>
-― Marilyn Monroe `,
+--Marilyn Monroe `,
   `“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”<br>
-― Albert Einstein `,
+-- Albert Einstein `,
   `“So many books, so little time.”<br>
-― Frank Zappa `,
+--Frank Zappa `,
   `“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”<br>
-― Bernard M. Baruch `,
+-- Bernard M. Baruch `,
   `“You've gotta dance like there's nobody watching,
 Love like you'll never be hurt,
 Sing like there's nobody listening,
 And live like it's heaven on earth.”<br>
-― William W. Purkey `,
+-- William W. Purkey `,
 ];
 function queteGen() {
   var randomQoute = Math.floor(Math.random() * quotes.length);
